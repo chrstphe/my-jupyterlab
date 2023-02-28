a8ac9aa64a9ae6e7fc073b3c707a8b1b81d7b243
Christophe Carvenius <info@svenskapop.se>
2023-02-28 11:19:55 +0100

    Added an env variable for local notebook dir

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker build -t my-jupyterlab .
-docker run -v ~/notebooks_dir:/notebooks_dir \
+docker run -v $NOTEBOOK_DIR:/notebooks_dir \
 	   -p 8888:8888 \
            -e GOOGLE_APPLICATION_CREDENTIALS=/install/google.json \
            -it my-jupyterlab
